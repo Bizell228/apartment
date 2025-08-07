babcd68daa6366d88abf2746567a3cb35187ca6c
Igor <igorsheredeko@gmail.com>
2025-08-07 23:51:24 +0300

    redactored config and started developing cian parser on selenium

--- config/public_config.py
+++ config/public_config.py
@@ -2,3 +2,7 @@ class BaseConfig:
     URL = "https://www.halooglasi.com/nekretnine/izdavanje-stanova/beograd"
     MAX_PAGES = 150
     WAIT_TIMEOUT = 15
+    CHROMEDRIVER_PATH = "/home/Igor/programs/chromedriver-linux64/chromedriver"
+    CHROMIUM_PATH = "/usr/bin/chromium"
+    OUTPUT_PARSER_PATH = "results/results.csv"
+    OUTPUT_NORM_PATH = "results/resultsNorm.csv"
--- test.py
+++ test.py
@@ -1,9 +1,15 @@
-import requests
-from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException, TimeoutException, StaleElementReferenceException
+import time
+import pandas as pd
+from tqdm import tqdm
+import logging
 import collections
-import csv
-import transliterate
-import re
 from config.public_config import BaseConfig
 
 apartmentInfo = collections.namedtuple(
@@ -26,15 +32,18 @@ apartmentInfo = collections.namedtuple(
     ]
 )
 
-max_pages = BaseConfig.MAX_PAGES
+logger = logging.getLogger("apartment_parser")
+logger.setLevel(logging.INFO)  
+handler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
 class CianPasrer:
+    config = BaseConfig()
+    max_pages = config.MAX_PAGES
+
     def __init__(self):
-        self.session = requests.Session()
-        self.sesion.headers = {
-            'User-Agent' : f'Mozilla/5.0 (X11; Linux x86_64; rv:139.0) Gecko/20100101 Firefox/139.0', 
-            'Accept-Language': 'ru'
-        }
         self.result = []
         self.result.append(apartmentInfo(
             price = 'price',
@@ -53,19 +62,47 @@ class CianPasrer:
             link = 'link'
              ))
 
-    #Loading page    
-    def load_page(self, i=1):
-        pass
+    def create_driver(config):
+        options = Options()
+        options.binary_location = config.CHROMIUM_PATH
+        service = Service(config.CHROMEDRIVER_PATH)
+        return webdriver.Chrome(
+            service=service, 
+            options=options
+        )
 
-    #will get html
+    #Loading page and extracting html from it.
+    def load_page(self, i=1):
+        driver = self.create_driver(self.config)
+        wait = WebDriverWait(driver, self.config.WAIT_TIMEOUT)
+
+
+
+    #Will pfrse html that we got in load function.
     def parse_page(self, html: str):
         pass
-    
-    #parse
+    #parses insides of every offer using parsed url
     def parse_page_offer(self, html_offer):
         pass
     
-    #
+    #parses every "card" listed on a page
     def parse_block(self, block):
         pass
+        
 
+if __name__ == "__main__":
+    options = Options()
+    config = BaseConfig()
+    options.binary_location = config.CHROMIUM_PATH
+    service = Service(config.CHROMEDRIVER_PATH)
+    driver = webdriver.Chrome(
+        service=service, 
+        options=options
+    )
+    driver.get("https://www.cian.ru/cat.php?deal_type=rent&engine_version=2&offer_type=flat&region=1&room1=1&room2=1&room3=1&room4=1&room5=1&room6=1&room7=1&room9=1&type=4")
+    time.sleep(3)
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(3)
+    title = driver.find_element(By.CSS_SELECTOR, "Title")
+    time.sleep(3)
+    print(title.get_attribute("outerHTML"))
